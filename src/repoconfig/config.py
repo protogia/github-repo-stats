b953d692ba07eb4f7b71db86797e6bba50bbb13e
protogia <goldengrizz01@gmail.com>
2026-03-17 11:28:05 +0100

    change var-name

--- src/repoconfig/config.py
+++ src/repoconfig/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ACCESS_TOKEN    = os.getenv("GITHUB_TOKEN")
+ACCESS_TOKEN    = os.getenv("GITHUB_ACCESS_TOKEN")
 USER            = "protogia"
 HISTORY_FILE    = "traffic_history.csv"
 PLOT_DIR        = "../static/plotly/home" # one dir down, because I use the script as submodule for my website
